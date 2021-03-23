6ea50ac855fae8a7501cb5f6ab9b1d02f7a3fd00
andresnsn <59550765+andresnsn@users.noreply.github.com>
2021-03-22 21:54:07 -0300

    Adicionando tela de leitura e funcionalidade do modo noturno

--- app/src/main/java/com/example/helloteca/MainActivity.java
+++ app/src/main/java/com/example/helloteca/MainActivity.java
@@ -3,17 +3,32 @@ package com.example.helloteca;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.Window;
-import android.view.WindowManager;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        /*
-        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);*/
         this.setContentView(R.layout.activity_main);
+        View view = findViewById(R.id.full_background);
+        Switch switchButton = findViewById(R.id.switch_button);
+
+        switchButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    view.setBackgroundColor(0xFF000000);
+                    setTheme(android.R.style.Theme_Black);
+
+                }
+                else{
+                    view.setBackgroundResource(R.drawable.reading_screen_gradient);
+                }
+            }
+        });
+
     }
 }
